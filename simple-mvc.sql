a3df11d4e0ff4c30cc5652be23c539a20a24a31d
Tolrep <pierlotedouard@gmail.com>
2018-04-10 17:23:59 +0200

    cestbon

--- simple-mvc.sql
+++ simple-mvc.sql
@@ -29,7 +29,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `item` (
   `id` int(11) UNSIGNED NOT NULL,
   `title` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Contenu de la table `item`
